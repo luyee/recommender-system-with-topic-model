bc17baa8d7634218fd0fda4a6ede8a6e95cf908d
Haibin <lauhaibin@gmail.com>
2012-01-12 23:44:42 -0500

    copyright added

--- src/rs/topics/LDAModel.java
+++ src/rs/topics/LDAModel.java
@@ -1,3 +1,11 @@
+/* This class is modified from MALLET software. Please see below for more 
+   copyright information.
+   Copyright (C) 2005 Univ. of Massachusetts Amherst, Computer Science Dept.
+   This file is part of "MALLET" (MAchine Learning for LanguagE Toolkit).
+   http://www.cs.umass.edu/~mccallum/mallet
+   This software is provided under the terms of the Common Public License,
+   version 1.0, as published by http://www.opensource.org.  For further
+   information, see the file `LICENSE' included with this distribution. */
 package rs.topics;
 
 import java.io.*;
